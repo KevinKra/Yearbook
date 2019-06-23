0d6b454a9e230304c14f2142d3792b1f4261f0a2
Kevin Krato <kratok08@gmail.com>
2019-06-23 10:11:16 -0600

    Add toggleable Form component and pass input

---
+++ src/components/Form/Form.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import "./Form.scss";
+class Form extends Component {
+  state = {
+    name: "",
+    quote: "",
+    superlative: "",
+    photo: ""
+  };
+
+  captureInput = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="Form sticky">
+        <form className="form-content">
+          <label htmlFor="name">
+            Name:
+            <input type="text" name="name" onChange={this.captureInput} />
+          </label>
+          <label htmlFor="quote">
+            Quote:
+            <input type="text" name="quote" onChange={this.captureInput} />
+          </label>
+          <label htmlFor="superlative">
+            Superlative:
+            <input
+              type="text"
+              name="superlative"
+              onChange={this.captureInput}
+            />
+          </label>
+          <label htmlFor="photo">
+            Photo:
+            <input type="photo" name="photo" onChange={this.captureInput} />
+          </label>
+          <button onClick={e => this.props.updateStudents(this.state, e)}>
+            Submit
+          </button>
+          <button>Exit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
