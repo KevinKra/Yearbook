bc0292093d02ba0eedbb7fe3b4303963865d7506
Kevin Krato <kratok08@gmail.com>
2019-06-23 09:03:02 -0600

    Destructure and pass in data

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import "./Person.css";
+import "./Person.scss";
 
 class Person extends Component {
   render() {
     const { name, photo, quote, superlative } = this.props.member;
     return (
       <article className="Person">
-        <h3>Person</h3>
         <h3>{name}</h3>
         <img src={photo} alt={name} />
         <p>{quote}</p>
