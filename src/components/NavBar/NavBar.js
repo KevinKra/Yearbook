f16ec9b0fb1e44a731e86ced7503bce5bc7ddcde
Kevin Krato <kratok08@gmail.com>
2019-06-23 12:51:46 -0600

    Conditionally render nav buttons

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,54 @@
 import React, { Component } from "react";
+import "./NavBar.scss";
 
 class NavBar extends Component {
   render() {
+    let toggleBoard;
+    if (this.props.displayStaff) {
+      toggleBoard = (
+        <form
+          className="toggleBoard"
+          onClick={e => this.props.toggleRender("displayStaff", e)}
+        >
+          <button className="disabled" disabled={true}>
+            Staff
+          </button>
+          <button>Students</button>
+        </form>
+      );
+    } else {
+      toggleBoard = (
+        <form
+          className="toggleBoard"
+          onSubmit={e => this.props.toggleRender("displayStaff", e)}
+        >
+          <button>Staff</button>
+          <button className="disabled" disabled={true}>
+            Students
+          </button>
+        </form>
+      );
+    }
     return (
       <nav className="NavBar">
         <h1>Turing Yearbook</h1>
-        <div>
-          <button onClick={this.props.toggleForm}>Add a new student?</button>
+        <div className="nav-btns">
+          {!this.props.displayStaff && (
+            <button onClick={e => this.props.toggleRender("displayForm", e)}>
+              Add a new student?
+            </button>
+          )}
+          {toggleBoard}
         </div>
       </nav>
     );
   }
+}
+
+{
+  /* <button onClick={() => this.props.toggleRender("displayStaff")}>
+{this.props.displayStaff ? <p>Students</p> : <p>Staff</p>}
+</button> */
 }
 
 export default NavBar;
