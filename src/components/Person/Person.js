1906d5c936a75e1907d2124cee466c7390a7dc44
Kevin Krato <kratok08@gmail.com>
2019-06-24 15:51:45 -0600

    Convert Person to functional component

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,22 +1,20 @@
 import React, { Component } from "react";
 import "./Person.scss";
 
-class Person extends Component {
-  render() {
-    const { name, photo, quote, superlative } = this.props.member;
-    return (
-      <article className="Person">
-        <header className="card-header">
-          <h2>{name}</h2>
-          <img src={photo} alt={name} />
-          <p>{quote}</p>
-        </header>
-        <main className="card-body">
-          <p>{superlative}</p>
-        </main>
-      </article>
-    );
-  }
-}
+const Person = props => {
+  const { name, photo, quote, superlative } = props.member;
+  return (
+    <article className="Person">
+      <header className="card-header">
+        <h2>{name}</h2>
+        <img src={photo} alt={name} />
+        <p>{quote}</p>
+      </header>
+      <main className="card-body">
+        <p>{superlative}</p>
+      </main>
+    </article>
+  );
+};
 
 export default Person;
