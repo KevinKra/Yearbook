9b40a34c98568305e061b1d91760ffda7ad39012
Kevin Krato <kratok08@gmail.com>
2019-06-28 07:26:37 -0600

    Conditionally render cards

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -3,18 +3,31 @@ import "./Person.scss";
 
 const Person = props => {
   const { name, photo, quote, superlative } = props.member;
-  return (
-    <article className="Person">
-      <header className="card-header">
-        <h2>{name}</h2>
-        <img src={photo} alt={name} />
-        <p>{quote}</p>
-      </header>
-      <main className="card-body">
-        <p>{superlative}</p>
-      </main>
-    </article>
-  );
+  let card;
+  if (quote && superlative) {
+    card = (
+      <article className="Person">
+        <header className="card-header">
+          <h2>{name}</h2>
+          <img src={photo} alt={name} />
+          <p>{quote}</p>
+        </header>
+        <main className="card-body">
+          <p>{superlative}</p>
+        </main>
+      </article>
+    );
+  } else {
+    card = (
+      <article className="Person">
+        <main className="card-header-alt">
+          <h2>{name}</h2>
+          <img className="fill-image" src={photo} alt={name} />
+        </main>
+      </article>
+    );
+  }
+  return <article>{card}</article>;
 };
 
 export default Person;
