4a43c2bd2bcbda02e261c087bc5bac822f1326ea
Kevin Krato <kratok08@gmail.com>
2019-06-28 06:53:35 -0600

    Convert to functional component

--- src/components/Cohort/Cohort.js
+++ src/components/Cohort/Cohort.js
@@ -1,23 +1,15 @@
-import React, { Component } from "react";
-import Person from "../Person/Person";
+import React from "react";
+import * as helpers from "../../utils";
 import "./Cohort.scss";
 
-class Cohort extends Component {
-  displayPeople = data => {
-    const keys = Object.keys(data);
-    return keys.map(key => <Person member={data[key]} key={data[key].id} />);
-  };
-
-  render() {
-    return (
-      <section className="Cohort">
-        {this.props.data.displayStaff &&
-          this.displayPeople(this.props.data.currentPage)}
-        {!this.props.data.displayStaff &&
-          this.displayPeople(this.props.data.currentPage)}
-      </section>
-    );
-  }
-}
+const Cohort = props => {
+  return (
+    <section className="Cohort">
+      {props.data.displayStaff && helpers.displayPeople(props.data.currentPage)}
+      {!props.data.displayStaff &&
+        helpers.displayPeople(props.data.currentPage)}
+    </section>
+  );
+};
 
 export default Cohort;
