b695e095a5085c9a9e255add1bb7cbf1f10c41ff
Kevin Krato <kratok08@gmail.com>
2019-06-28 07:03:41 -0600

    Minor refactor

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -29,7 +29,7 @@ class Pagination extends Component {
   };
 
   render() {
-    return <div>{this.renderButtons()}</div>;
+    return <section>{this.renderButtons()}</section>;
   }
 }
 
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Person.scss";
 
 const Person = props => {
